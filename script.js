8b95f94ec4f17628eacef2c13cf84289c9ebd476
irfandzaky97 <irfandzaky97@gmail.com>
2024-02-21 01:50:01 +0700

    Perbaiki Container Porject

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function toggle(){
+    var demo = document.querySelector(".demo");
+    var video = document.querySelector("video");
+    demo.classList.toggle("active");
+    video.pause();
+    video.currentTime = 0;
+}
+
+function toggleVideo(videoSrc) {
+    var demo = document.querySelector(".demo");
+    demo.classList.toggle("active");
+
+    var videoPlayer = document.getElementById('videoPlayer');
+    if (videoSrc) {
+        videoPlayer.src = videoSrc;
+        videoPlayer.load();
+        videoPlayer.play();
+    } else {
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
+    }
+}
